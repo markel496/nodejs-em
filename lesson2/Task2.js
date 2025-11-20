9655e05cc6a3f848bd0cd1979e0023ed330f506e
Ivan <markelov.ivan1996@yandex.ru>
2025-11-20 16:19:24 +0300

    Выполнение практики по теме 'Продвинутые условные операторы'

--- lesson2/Task2.js
+++ lesson2/Task2.js
@@ -1,3 +1,5 @@
-const a = 0;
+const a = 0
 const b = 0
-const c = ...
+const c = a + b > 100
+
+console.log(c ? 'Да' : 'Нет')
--- lesson2/Task3.js
+++ lesson2/Task3.js
@@ -1,5 +1,15 @@
-const a = true;
-const b = true;
-const c = true;
+const a = true
+const b = true
+const c = true
 
-const ...
+/** С использованием if else */
+let res
+
+if (c) {
+  res = a || b
+} else {
+  res = a && b
+}
+
+/** С использованием тернарного оператора */
+const result = c ? a || b : a && b
