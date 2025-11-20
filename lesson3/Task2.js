324bd165cd0e323001323ebcd1051b37025bd503
Ivan <markelov.ivan1996@yandex.ru>
2025-11-20 21:14:25 +0300

    Выполнение практики по теме 'Методы работы с массивами'

--- lesson3/Task2.js
+++ lesson3/Task2.js
@@ -1,4 +1,6 @@
-const array = [2,3,4,1,2,3,4,5]
+const array = [2, 3, 4, 1, 2, 3, 4, 5]
 
 // Код, который проверит, является ли массив отсортированным
-let isSortedArray = ...
+let isSortedArray = array.every((el, i) => i === 0 || el >= array[i - 1])
+
+console.log(isSortedArray) // false
