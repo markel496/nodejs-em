07666b424ba5a086d6b4fe6b31339be7b0226480
Ivan <markelov.ivan1996@yandex.ru>
2025-11-21 17:14:42 +0300

    Выполнение практики по теме 'Методы работы с объектами'

--- lesson4/Task2.js
+++ lesson4/Task2.js
@@ -1,4 +1,12 @@
+/** Нужно написать код, который оставит в объекте только свойства, которые являются четными числами. Исходные объект изменять нельзя: */
+
 const obj = { prop1: 1, prop2: 2, prop3: true, prop4: 'test', prop5: 10 }
-let resultObj = {}
-...
-console.log(resultObj); // { prop2: 2, prop5: 10 }
+const resultObj = {}
+
+for (const key in obj) {
+  if (typeof obj[key] === 'number' && !(obj[key] % 2)) {
+    resultObj[key] = obj[key]
+  }
+}
+
+console.log(resultObj) // { prop2: 2, prop5: 10 }
