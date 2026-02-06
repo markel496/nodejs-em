22b01a4690ff755468b7790f43c6d93c706c5a6e
Ivan <markelov.ivan1996@yandex.ru>
2026-02-06 19:49:58 +0300

    Введение Express/Koa

---
+++ nodejs/App.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+
+const PORT = 3000
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.post((req, res) => {
+  res.send('POST request to the homepage')
+})
+
+app.use(router)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
